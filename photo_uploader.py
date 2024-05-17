d47b970c449a03b8af6dba5a9d02f015c7121c32
Daniel Loera <danny.reatret@gmail.com>
2024-05-16 21:08:07 -0500

    Add exif parsing

--- photo_uploader.py
+++ photo_uploader.py
@@ -5,7 +5,7 @@ from appwrite.id import ID
 from appwrite.input_file import InputFile
 from os import listdir, remove
 from os.path import isfile, join
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ExifTags
 import secret
 
 VALID_EXTENSIONS = {'jpg', 'png'}
@@ -40,9 +40,35 @@ class AppWriteHelper:
             permissions = ["read(\"any\")"] # optional
         )
 
+def nullable_float(val):
+    return val if val is None else float(val)
+
+def nullable_str(val):
+    return val if val is None else str(val)
+
+def nullable_int(val):
+    return val if val is None else int(val)
+
 def is_valid_file(file_path):
     file_ext = file_path.split('.')[-1].lower()
     return isfile(file_path) and (file_ext in VALID_EXTENSIONS)
+
+def parse_exif(img_exif):
+    IFD_CODE_LOOKUP = {i.value: i.name for i in ExifTags.IFD}
+    exif_map = {}
+
+    for tag_code, value in img_exif.items():
+        # if the tag is an IFD block, nest into it
+        if tag_code in IFD_CODE_LOOKUP:
+            ifd_tag_name = IFD_CODE_LOOKUP[tag_code]
+            exif_map[ifd_tag_name] = tag_code
+            ifd_data = img_exif.get_ifd(tag_code).items()
+            for nested_key, nested_value in ifd_data:
+                nested_tag_name = ExifTags.GPSTAGS.get(nested_key, None) or ExifTags.TAGS.get(nested_key, None) or nested_key
+                exif_map[nested_tag_name] = nested_value
+        else:
+            exif_map[ExifTags.TAGS.get(tag_code)] = value
+    return exif_map
 
 def main():
     photo_folder_path = input("Image folder to upload: ")
@@ -55,6 +81,7 @@ def main():
 
         thumbnail_path = f'{photo_folder_path}/thumbnail_{photo}'
         image = Image.open(full_path)
+        exif = parse_exif(image.getexif())
         image = ImageOps.exif_transpose(image)
         width, height = image.size
         # downsize the image with an LANCZOS filter (gives the highest quality)
@@ -75,7 +102,17 @@ def main():
             'width': width,
             'height': height,
             'full_res_url': full_url,
-            'thumbnail_url': thumbnail_url
+            'thumbnail_url': thumbnail_url,
+            'shutter_speed': nullable_float(exif.get('ShutterSpeedValue', None)),
+            'focal_length': nullable_float(exif.get('FocalLength', None)),
+            'exposure_time': nullable_float(exif.get('ExposureTime', None)),
+            'f_number': nullable_float(exif.get('FNumber', None)),
+            'iso': nullable_int(exif.get('ISOSpeedRatings', None)),
+            'lens_make': nullable_str(exif.get('LensMake', None)),
+            'lens_model': nullable_str(exif.get('LensModel', None)),
+            'camera_make': nullable_str(exif.get('Make', None)),
+            'camera_model': nullable_str(exif.get('Model', None)),
+            'date': nullable_str(exif.get('DateTime', None)),
             }
         )
         remove(thumbnail_path)
